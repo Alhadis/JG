ecbebaad042215958988af5f184211d7363e44b8
Alhadis <gardnerjohng@gmail.com>
2020-01-21 18:55:55 +1100

    Add support for `@enum` type-declarations
    Also add some damn documentation so I can read my own code next time.

--- jsdoc/to-typescript.mjs
+++ jsdoc/to-typescript.mjs
@@ -3,11 +3,31 @@ import {readFileSync}  from "fs";
 import {dirname, join} from "path";
 import {fileURLToPath} from "url";
 
+/**
+ * Extract a list of TypeScript declarations from JSDoc blocks.
+ * @param {String} inputFile
+ * @return {Promise.<TypeDeclaration[]>}
+ * @public
+ */
 export async function extractTypes(inputFile){
+	/**
+	 * @typedef  {Object} TypeDeclaration
+	 * @property {String} body
+	 * @property {String} name
+	 * @property {String} type
+	 * @property {Object} doclet
+	 */
 	const {doclets} = await loadFile(inputFile);
 	const types = [];
 	for(const doc of doclets){
-		switch(doc.kind){
+		if(doc.isEnum)
+			types.unshift({
+				body: `export declare enum ${doc.name} {${parseEnum(doc)}}`,
+				name: doc.name,
+				type: "enum",
+				doclet: doc,
+			});
+		else switch(doc.kind){
 			case "function":
 				doc.memberof || types.push({
 					body: `export declare function ${doc.name}${parseFunction(doc)};`,
@@ -46,13 +66,23 @@ export async function extractTypes(inputFile){
 	return types;
 }
 
-export async function loadFile(path){
+
+/**
+ * Load and parse a file using JSDoc.
+ *
+ * @param {String} path
+ * @param {Boolean} [ignoreErrors=false]
+ * @return {{doclets: Object[], source: String}}
+ */
+export async function loadFile(path, ignoreErrors = false){
 	const dir = dirname(fileURLToPath(import.meta.url));
 	const result = await exec("jsdoc", ["-c", join(dir, "config.json"), "-X", path]);
 	
 	// Complain loudly if JSDoc reported an error
-	result.stderr && process.stderr.write(`jsdoc: ${result.stderr}`);
-	result.code   && process.exit(result.code);
+	if(!ignoreErrors){
+		result.stderr && process.stderr.write(`jsdoc: ${result.stderr}`);
+		result.code   && process.exit(result.code);
+	}
 	
 	const source = readFileSync(path, "utf8");
 	const doclets = JSON.parse(result.stdout)
@@ -70,6 +100,59 @@ export async function loadFile(path){
 	return {doclets, source};
 }
 
+
+/**
+ * Parse an {@link @enum|https://jsdoc.app/tags-enum.html} tag and
+ * return the equivalent TypeScript syntax. Note that computed values
+ * (such as object expressions) may yield invalid results.
+ *
+ * @example parseEnum({
+ *    isEnum: true,
+ *    kind: "member",
+ *    name: "States",
+ *    type: {names: ["number"]},
+ *    properties: [
+ *       {name: "ON",  defaultvalue: 1, type: {names: ["number"]}},
+ *       {name: "OFF", defaultvalue: 0, type: {names: ["number"]}},
+ *    ],
+ * }) == "ON = 1, OFF = 0";
+ *
+ * @param {Object} obj
+ * @return {String}
+ */
+export function parseEnum(obj){
+	const props = [];
+	for(const prop of obj.properties){
+		const {name, defaultvalue, meta} = prop;
+		if(!("defaultvalue" in prop))
+			throw new TypeError(`Missing value for ${name} in enum ${obj.name}`);
+		const isStr = "string" === typeof defaultvalue && "Literal" === meta.code.type;
+		const value = isStr ? '"' + defaultvalue + '"' : defaultvalue;
+		props.push(`${name} = ${value}`);
+	}
+	return props.join(", ");
+}
+
+
+/**
+ * Parse a {@link @typedef|https://jsdoc.app/tags-typedef.html} tag into
+ * a type annotation or type declaration, depending on whether `decl` is
+ * enabled.
+ *
+ * @example <caption>Parsing <code>@typedef {number|string} Numeric</code></caption>
+ * const doclet = {
+ *    kind: "typedef",
+ *    name: "Numeric",
+ *    scope: "global",
+ *    type: {names: ["number", "string"]},
+ * };
+ * parseTypedef(doclet, true) == "declare type Numeric = number | string;"
+ * parseTypedef(doclet, false) == "Numeric: number | string;"
+ * 
+ * @param {Object} obj
+ * @param {Boolean} [decl=true]
+ * @return {String}
+ */
 export function parseTypedef(obj, decl = true){
 	const isObject = /^Object(?:$|[.<])/i;
 	const hasProps = obj.properties && obj.properties.length;
@@ -95,6 +178,15 @@ export function parseTypedef(obj, decl = true){
 		: `${obj.name}: ${type};`;
 }
 
+
+/**
+ * Parse a {@link @property|https://jsdoc.app/tags-property.html} tag into an
+ * annotated object member.
+ *
+ * @example parseProp({name: "foo", type: {names: ["String"]}}) == "foo: string;";
+ * @param {Object} obj
+ * @return {String}
+ */
 export function parseProp(obj){
 	return (obj.readonly ? "readonly " : "")
 		+ obj.name
@@ -103,6 +195,28 @@ export function parseProp(obj){
 		+ ";";
 }
 
+
+/**
+ * Parse a function and annotate its parameters and return-type.
+ *
+ * @example <caption>Parsing <code>function doStuff(foo, bar)</code></caption>
+ * const doclet = {
+ *    kind: "function",
+ *    name: "doStuff",
+ *    params: [
+ *       {type: {names: ["Boolean"]}, name: "foo"},
+ *       {type: {names: ["Boolean"]}, name: "bar"},
+ *    ],
+ *    returns: [{type: {names: ["String"]}}],
+ *    scope: "global",
+ * };
+ * parseFunction(doclet)         == "(foo: boolean, bar: boolean): string";
+ * parseFunction(doclet, " => ") == "(foo: boolean, bar: boolean) => string";
+ *
+ * @param {Object} obj
+ * @param {String} [sep=": "]
+ * @return {String}
+ */
 export function parseFunction(obj, sep = ": "){
 	const nested = {};
 	const params = (obj.params || []).map(arg => {
@@ -116,28 +230,62 @@ export function parseFunction(obj, sep = ": "){
 	return `(${params.join(", ")})${sep + parseReturnType(obj)}`;
 }
 
+
+/**
+ * Parse a function parameter.
+ *
+ * @example parseParam(jsdoc("@param {?Array.<String>} foo")) == "foo?: string[]";
+ * @param {Object} obj
+ * @param {?Object[]} [props=null]
+ * @return {String}
+ */
 export function parseParam(obj, props = null){
 	let result = obj.variable
 		? ("..." + obj.name)
-		: obj.name + (obj.optional ? "?" : "");
+		: obj.name + (obj.optional || obj.nullable ? "?" : "");
 	result += ": " + (props
 		? `{${props.map(p => parseParam(p)).join("; ")}}`
 		: parseType(obj.type, obj.variable));
 	return result;
 }
 
+
+/**
+ * Parse a function's return type.
+ *
+ * @example <caption>Parsing <code>@return {Number[]|String}</code></caption>
+ * parseReturnType({
+ *    async: true,
+ *    kind: "function",
+ *    name: "loadString",
+ *    returns: [{type: {names: ["Array.<Number>", "String"]}}],
+ * }) == "Promise<number[] | string>";
+ *
+ * @param {Object}
+ * @return {String}
+ */
 export function parseReturnType(obj){
 	if(!obj || !obj.returns)
 		return (obj && obj.async) ? "Promise<void>" : "void";
 	const names = new Set();
 	for(const {type} of obj.returns)
-		names.add(...type.names);
+		type.names.map(name => names.add(name));
 	let type = names.size ? parseType({names: [...names]}) : "void";
 	if(obj.async && !/^Promise</.test(type))
 		type = `Promise<${type}>`;
 	return type;
 }
 
+
+/**
+ * Parse a JSDoc {@link @type|https://jsdoc.app/tags-type.html} tag
+ * and return the equivalent TypeScript syntax.
+ *
+ * @example parseType(jsdoc("@type Array.<Boolean>")) == "boolean[]";
+ * @param {Object} obj
+ * @param {Boolean} [variadic=false]
+ * @return {String}
+ */
 export function parseType(obj, variadic = false){
 	if("string" === typeof obj)
 		return parseType({names: [obj]}, variadic);
