071ac749f20e9082b443525a5b48ddcc26bed141
Alhadis <gardnerjohng@gmail.com>
2026-08-06 20:11:38 +1000

    Expand filename coverage of jg-path(1)

--- bin/path.mjs
+++ bin/path.mjs
@@ -33,32 +33,62 @@ if(process.argv[1] === path || globalThis.$0 === path){
  *
  * @example getPath("eslint") == "/path/to/jg/eslint/.eslintrc.json";
  * @param {String} filename
- * @return {String|null} An absolute path if the file exists; otherwise, null.
+ * @return {?String} An absolute path if the file exists; otherwise, null.
  * @public
  */
 export default function getPath(filename){
 	
-	// Shorthand: ESLint
-	let match = filename.match(/^\.?eslint(?:rc|[-/]?config)?\/?(?:\b(atom|babel|mw)(?:\.js)?\/?|(?:\.json)|(es5|typescript(?:\.full)?)(?:\.json)?)$/i);
+	// ESLint
+	let match = filename.match(/^\.?eslint(?:rc|[-/]?config)?\/?(?:\b(atom|babel|mw)(?:\.js)?\/?|(?:\.json)|(es5|typescript(?:\.full)?)(?:\.json)?)?$/i);
 	if(null !== match){
 		const file = match[1] ? `${match[1]}.js` : match[2] ? `${match[2]}.json` : ".eslintrc.json";
 		return resolve(join(ROOT_DIR, "eslint", file));
 	}
 	
-	// Shorthand: TSLint (now ESLint with TypeScript-specific config)
+	// TSLint (now ESLint with TypeScript-specific config)
 	match = filename.match(/^\.?tslint(?:rc|[-/]?config)?(?:\.json)?$/i);
 	if(null !== match)
 		return resolve(join(ROOT_DIR, "eslint", "typescript.full.json"));
 
-	// Shorthand: CoffeeLint
+	// CoffeeLint
 	match = filename.match(/^\.?coffeelint(?:rc|[-/]?config)?(?:\.json)?$/i);
 	if(null !== match)
 		return resolve(join(ROOT_DIR, "etc", "coffeelint.json"));
 	
-	// Shorthand: JSDoc config
-	match = filename.match(/^\.?jsdoc(?:rc)?\.(?:conf|json|cfg)$/i);
+	// JSDoc
+	match = filename.match(/^\.?jsdoc(?:rc)?\.?(?:conf|json|cfg)?$/i);
 	if(null !== match)
 		return resolve(join(ROOT_DIR, "jsdoc", "config.json"));
+	
+	// Clang-format
+	match = filename.match(/^\.?(?:clang|llvm)(?:-?(?:format|fmt))?$/i);
+	if(null !== match)
+		return resolve(join(ROOT_DIR, "etc", ".clang-format"));
+
+	// Prettier
+	match = filename.match(/^\.?(?:prettier|pretty)(?:rc)?\.?(?:json)?$/i);
+	if(null !== match)
+		return resolve(join(ROOT_DIR, "etc", ".prettierrc.json"));
+	
+	// Dprint
+	match = filename.match(/^\.?dprint\.?(?:json)?$/i);
+	if(null !== match)
+		return resolve(join(ROOT_DIR, "etc", "dprint.json"));
+	
+	// Rustfmt
+	match = filename.match(/^\.?rust[_-]?(?:fmt|format)\.?(?:toml|json|ya?ml)?$/i);
+	if(null !== match)
+		return resolve(join(ROOT_DIR, "etc", "rustfmt.toml"));
+	
+	// Headless Firefox profile
+	match = filename.match(/(?:^|\/)(?:etc\/)?(?:profiles?\/)?firefox[-_]?(?:headless|profile)?(?:\.?m?js)?$|^ff$/i);
+	if(null !== match)
+		return resolve(join(ROOT_DIR, "etc", "profiles", "firefox-headless.js"));
+	
+	// Wrapper/front-end
+	match = filename.match(/^\.?index\.?(?:[cmt]?js)?$|^(?:entry|main|self)$/);
+	if(null !== match)
+		return resolve(join(ROOT_DIR, "index.mjs"));
 	
 	// No argument: Installation directory
 	if(!filename)
